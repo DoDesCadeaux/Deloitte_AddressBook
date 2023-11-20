3d9882f4ca080d173b0684b187370655c986c732
DoDesCadeaux <dduraku@student.s19.be>
2023-11-20 15:53:46 +0100

    Creating Person class with attributes and accessors and respecting the encapsulation

--- src/AddressBookApp.java
+++ src/AddressBookApp.java
@@ -1,5 +1,38 @@
+import java.util.Date;
+
+class Person {
+    //Attributes
+    private String _name;
+    private String _gender;
+    private Date _birthday;
+
+    //Setters (Sets attributes from accessors for encapsulation purpose)
+    public void set_name(String _name) {
+        this._name = _name;
+    }
+
+    public void set_gender(String _gender) {
+        this._gender = _gender;
+    }
+
+    public void set_birthday(Date _birthday) {
+        this._birthday = _birthday;
+    }
+
+    public String get_name() {
+        return _name;
+    }
+
+    public String get_gender() {
+        return _gender;
+    }
+
+    public Date get_birthday() {
+        return _birthday;
+    }
+}
+
 public class AddressBookApp {
     public static void main(String[] args) {
-        System.out.println("Starting Project");
     }
 }
